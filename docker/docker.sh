8a507c9e5fa2ee38541ee5da149178d0a38c75e5
Subin Sapkota <subinsapkota@gmail.com>
2017-04-01 11:39:48 -0600

    Travis config changes, continuing.

--- docker/docker.sh
+++ docker/docker.sh
@@ -25,7 +25,7 @@ cp docker-entrypoint.sh $TEMP
 
 cd $TEMP
 
-sftp -i "~/.ssh/testDockerInstance.pem" -o StringHostKeyChecking=no -b /dev/stdin ubuntu@52.41.69.145 <<EOF
+sftp -i $FILE1 -o StringHostKeyChecking=no -b /dev/stdin ubuntu@52.41.69.145 <<EOF
 cd Docker
 rm *
 put Dockerfile
@@ -35,7 +35,7 @@ put $ZIP
 exit
 EOF
 
-ssh -i "~/.ssh/testDockerInstance.pem" -o StringHostKeyChecking=no -t -t ubuntu@52.41.69.145 <<EOF
+ssh -i $FILE1 -o StringHostKeyChecking=no -t -t ubuntu@52.41.69.145 <<EOF
 cd Docker
 chmod 700 docker-entrypoint.sh
 docker rmi subin215/scala-project:1.0.1-SNAPSHOT
