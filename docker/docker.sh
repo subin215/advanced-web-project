696ee71f63295479009a79d01e69bd586dac24f6
Subin Sapkota <subinsapkota@gmail.com>
2017-04-01 11:13:42 -0600

    Travis config changes, continuing.

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,10 +4,19 @@ TEMP=$(mktemp -d)
 
 ZIP=project-1.0-SNAPSHOT.zip
 
+if [[ "$USER"!="subin" ]];
+then
+FILE1="testDockerInstance.pem"
+cd docker
+cp testDockerInstance.pem $TEMP
+cd ..
+else
+FILE1="~/.ssh/testDockerInstance.pem"
 # Package application
 cd ..
 sbt clean dist
 cd docker
+fi
 
 cp ../target/universal/$ZIP $TEMP
 cp Dockerfile $TEMP
@@ -16,7 +25,7 @@ cp docker-entrypoint.sh $TEMP
 
 cd $TEMP
 
-sftp -i "~/.ssh/testDockerInstance.pem" -b /dev/stdin ubuntu@52.41.69.145 <<EOF
+sftp -i "~/.ssh/testDockerInstance.pem" -o StringHostKeyChecking=no -b /dev/stdin ubuntu@52.41.69.145 <<EOF
 cd Docker
 rm *
 put Dockerfile
@@ -26,7 +35,7 @@ put $ZIP
 exit
 EOF
 
-ssh -i "~/.ssh/testDockerInstance.pem" -t -t ubuntu@52.41.69.145 <<EOF
+ssh -i "~/.ssh/testDockerInstance.pem" -o StringHostKeyChecking=no -t -t ubuntu@52.41.69.145 <<EOF
 cd Docker
 chmod 700 docker-entrypoint.sh
 docker rmi subin215/scala-project:1.0.1-SNAPSHOT
---
+++ docker/testBash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ "$USER"!="subin" ]];
+then
+echo "SUBIN"
+else
+echo "NOT SUBIN"
+fi
