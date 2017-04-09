6a65b225fe7d0ee170a364edfb1b0904f399ddfb
Subin Sapkota <subinsapkota@gmail.com>
2017-04-09 10:36:21 -0600

    Travis config changes.

--- docker/docker.sh
+++ docker/docker.sh
@@ -13,10 +13,10 @@ cd docker
 
 # MOVE REQUIRED FILES TO TEMP FOLDER
 cp ../target/universal/$ZIP $TEMP/$ZIP
-cp awsDockerInstance.pem.enc $TEMP/Dockerfile
-cp awsDockerInstance.pem $TEMP/Dockerfile
+cp awsDockerInstance.pem.enc $TEMP/awsDockerInstance.pem.enc
+cp awsDockerInstance.pem $TEMP/awsDockerInstance.pem
 chmod 700 $TEMP/awsDockerInstance.pem.enc
-chmod 700 $TEMP/awsDockerInstance.enc
+chmod 700 $TEMP/awsDockerInstance.pem
 cp Dockerfile $TEMP/Dockerfile
 cp docker-compose.yml $TEMP/docker-compose.yml
 cp docker-entrypoint.sh $TEMP/docker-entrypoint.sh
@@ -24,7 +24,7 @@ cp docker-entrypoint.sh $TEMP/docker-entrypoint.sh
 cd $TEMP
 
 # MOVE FILES TO REMOTE SERVER
-sftp -i $FILE1 -b /dev/stdin ubuntu@ec2-54-201-76-3.us-west-2.compute.amazonaws.com <<EOF
+sftp -i $FILE1 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -b /dev/stdin ubuntu@ec2-54-201-76-3.us-west-2.compute.amazonaws.com <<EOF
 cd Docker
 rm *
 put Dockerfile
@@ -35,7 +35,7 @@ exit
 EOF
 
 # START DOCKER CONTAINER FOR APPLICATION ON REMOTE SERVER
-ssh -i $FILE1 -t -t ubuntu@ec2-54-201-76-3.us-west-2.compute.amazonaws.com <<EOF
+ssh -i $FILE1 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t -t ubuntu@ec2-54-201-76-3.us-west-2.compute.amazonaws.com <<EOF
 cd Docker
 chmod +x docker-entrypoint.sh
 docker rmi subin215/scala-project:1.0.1-SNAPSHOT
